7e4c4210a6235999796704e083522e3a2f53a203
Ken Ishimoto <k.ishimoto@treasureboat.org>
2025-10-14 11:00:22 +0900

    fix shell scripts

---
+++ tb.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+echo "**********************************************************************"
+echo "***** starting 'tb.sh' script version : 2025/10/14"
+echo "***** TB Gradle/Maven Setup"
+echo "**********************************************************************"
+echo ""
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Update Wrapper"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+./mvnw wrapper:wrapper
+cd GradleDev
+./gradlew wrapper
+cd ..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Executing EO Template Installation"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+cd TBEOTemplates/
+sudo mkdir -p /usr/local/TreasureBoat
+sudo chmod 777 /usr/local/TreasureBoat
+cp *.vm /usr/local/TreasureBoat/.
+cd ..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Install TreasureBoat SuperPOM"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+cd SuperPOM/
+../mvnw install
+cd ..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Executing Gradle PlugIn Installation"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+cd GradleDev/gradle-tb-plugin
+./gradlew wrapper
+./gradlew clean
+./gradlew build
+./gradlew publishToMavenLocal
+ls -l ~/.m2/repository/org/treasureboat/gradle/gradle-tb-plugin/
+cd ../..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] DONE"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
---
+++ tbjenkins.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+echo "**********************************************************************"
+echo "***** starting 'tbjenkins.sh' script version : 2025/10/14"
+echo "***** TB Gradle/Maven Setup for Jenkins/Teamcity"
+echo "**********************************************************************"
+echo ""
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Update Wrapper"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+./mvnw wrapper:wrapper
+cd GradleDev
+./gradlew wrapper
+cd ..
+
+#echo "[INFO] ------------------------------------------------------------------------"
+#echo "[INFO] Executing EO Template Installation"
+#echo "[INFO] ------------------------------------------------------------------------"
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Install TreasureBoat SuperPOM"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+cd SuperPOM/
+../mvnw install
+cd ..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] Executing Gradle PlugIn Installation"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
+cd GradleDev/gradle-tb-plugin
+./gradlew wrapper
+./gradlew clean
+./gradlew build
+./gradlew publishToMavenLocal
+ls -l ~/.m2/repository/org/treasureboat/gradle/gradle-tb-plugin/
+cd ../..
+
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] DONE"
+echo "[INFO] ------------------------------------------------------------------------"
+echo "[INFO] "
